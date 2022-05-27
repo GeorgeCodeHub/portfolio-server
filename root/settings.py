36e83b8e17267f297064318e125132541e0581d4
GeorgeCodeHub <54906099+GeorgeCodeHub@users.noreply.github.com>
2022-05-28 00:20:37 +0300

    Automatic configuration with django-heroku

--- root/settings.py
+++ root/settings.py
@@ -10,11 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import django_heroku
+
 from pathlib import Path
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
+# Settings for heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
