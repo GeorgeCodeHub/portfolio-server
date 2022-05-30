892422c6b20f8df9ad47abb40e797d0b979359ed
GeorgeCodeHub <54906099+GeorgeCodeHub@users.noreply.github.com>
2022-05-30 23:57:11 +0300

    make fields editable for admin

--- api/admin.py
+++ api/admin.py
@@ -7,15 +7,24 @@ from .models import Certificate, Degree, Job, Project, Skill
 
 class CertificateAdmin(admin.ModelAdmin):
     list_display = ["id", "title", "school", "dateAcquired"]
+    list_editable = ["title", "school", "dateAcquired"]
 
 
 class DegreesAdmin(admin.ModelAdmin):
     list_display = ["id", "title", "school", "dateFrom", "dateTo"]
+    list_editable = ["title", "school", "dateFrom", "dateTo"]
 
 
 class JobsAdmin(admin.ModelAdmin):
     list_display = [
         "id",
+        "positionTitle",
+        "companyTitle",
+        "description",
+        "dateFrom",
+        "dateTo",
+    ]
+    list_editable = [
         "positionTitle",
         "companyTitle",
         "description",
@@ -35,10 +44,21 @@ class ProjectsAdmin(admin.ModelAdmin, DynamicArrayMixin):
         "technologies",
         "images",
     ]
+    list_editable = [
+        "id",
+        "title",
+        "isFeatured",
+        "description",
+        "githubURL",
+        "runningAppURL",
+        "technologies",
+        "images",
+    ]
 
 
 class SkillsAdmin(admin.ModelAdmin):
     list_display = ["id", "title", "technologies"]
+    list_editable = ["id", "title", "technologies"]
 
 
 admin.site.register(Certificate, CertificateAdmin)
