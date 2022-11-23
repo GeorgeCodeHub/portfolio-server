4f5481d0a4151614a8e0515d16a3b5a93c357203
GeorgeCodeHub <54906099+GeorgeCodeHub@users.noreply.github.com>
2022-11-24 00:19:30 +0200

    More settings for railway

--- root/settings.py
+++ root/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["https://web-production-29c4.up.railway.app"]
 
 # CORS rules and allowed external connections
 # ALLOWED_HOSTS = [
